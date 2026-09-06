1dc2402c59b0e7a9a5760c9d37481d909c6e1369
sital bhujel <sital.805424@cct.tu.edu.np>
2026-09-06 15:24:11 +0545

    Time: 5 ms (14.81%), Space: 59 MB (60.46%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -8,27 +8,23 @@ var minSubArrayLen = function(target, nums) {
     let low = 0;
     let high = 0;
     let length = Infinity;
-    let totalSum= 0;
-    for (let i = low;i<nums.length;i++){
-      totalSum+=nums[i];
-    }
+    let flag= 0;
     for(let i = low;i<=high;i++){
       sum+=nums[i];
     };
-    if(totalSum<target){
-      console.log("hello")
-      length = 0;
-      return length;
-    }
     while(high<nums.length){
       if(sum<target){
         high++;
+        if(high<nums.length){
+
         sum+=nums[high];
+        }
       } else {
+        flag=1
         length = Math.min((high-low+1),length)
         low++;
         sum-=nums[low-1]
       } 
     }
-    return length;
+    return flag===0?0:length;
 };
