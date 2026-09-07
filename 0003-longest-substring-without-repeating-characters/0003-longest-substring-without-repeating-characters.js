0e1cd472c572c58ee3116610702a4e6d75bf7973
sital bhujel <sital.805424@cct.tu.edu.np>
2026-09-08 00:26:08 +0545

    Time: 94 ms (34.27%), Space: 63.5 MB (16.56%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -5,15 +5,21 @@
 var lengthOfLongestSubstring = function(s) {
     let low = 0;
     let res = 0;
-    let str = [];
+    let hashMap = new Map();
     for(let high = 0; high<s.length;high++){
-      while(str.includes(s[high])){
-        str.shift();
+       hashMap.set(s[high],(hashMap.get(s[high]) ?? 0)+1);
+        let k = high - low + 1;
+      while(hashMap.size < k){
+        hashMap.set(s[low],(hashMap.get(s[low]) ?? 0)-1);
+        if((hashMap.get(s[low]) ?? 0) < 1){
+          hashMap.delete(s[low])
+        };
         low++;
+        k = high-low+1;
       }
-        let length = (high-low)+1;
-        res = Math.max(res,length)
-      str.push(s[high]);
+      if(hashMap.size === k){
+        res = Math.max(k,res)
+      }
       
     }
   return res
