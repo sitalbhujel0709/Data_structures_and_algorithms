8fe4ccbc53b94c8da04e4b3513e174e43186c225
sital bhujel <sital.805424@cct.tu.edu.np>
2026-09-10 23:27:12 +0545

    Time: 41 ms (37.56%), Space: 61.2 MB (55.76%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
@@ -3,31 +3,35 @@
  * @param {string} p
  * @return {number[]}
  */
-var findAnagrams = function(s, p) {
-    let frequencyArr1 = Array(256).fill(0);
-    let frequencyArr2 = Array(256).fill(0);
-    for(let i = 0;i<p.length;i++){
-      frequencyArr1[p[i].charCodeAt(0)]++;
+var findAnagrams = function (s, p) {
+  let frequencyArr1 = Array(256).fill(0);
+  let frequencyArr2 = Array(256).fill(0);
+  for (let i = 0; i < p.length; i++) {
+    frequencyArr1[p[i].charCodeAt(0)]++;
+  }
+  for (let i = 0; i < p.length; i++) {
+        frequencyArr2[s.charCodeAt(i)]++;
     }
-    let low = 0;
-    let high = p.length - 1;
-    console.log(high)
-    let res = [];
-    if(s.length<p.length){
-      return res;
-    }
-    const compare = (a,b)=>a.every((v,i)=>v===b[i]);
-    while(high<s.length){
-      for(let i = low;i<=high;i++){
-        frequencyArr2[s[i].charCodeAt(0)]++;
-      }
-      if(compare(frequencyArr1,frequencyArr2)){
-        res.push(low)
-      }
-      low++;
-      high++;
-      frequencyArr2 = Array(256).fill(0);
-    }
+  let low = 0;
+  let high = p.length - 1;
+  let res = [];
+  if (s.length < p.length) {
     return res;
+  }
+  const compare = (a, b) => a.every((v, i) => v === b[i]);
+  while (high < s.length) {
+    if (compare(frequencyArr1, frequencyArr2)) {
+      res.push(low);
+    }
+    high++;
+    if (high === s.length) {
+      break;
+    }
+    frequencyArr2[s.charCodeAt(high)]++;
+    frequencyArr2[s.charCodeAt(low)]--;
+    low++;
+  }
+  return res;
 };
+
 
